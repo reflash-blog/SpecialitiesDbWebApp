698c32ba9b8620f4c4b8ec5b7cf0d603f545111b
reflash <reflash-ne@yandex.ru>
2015-06-02 01:34:03 +0300

    commit#4

---
+++ StudyDirectionDbWebApp/Controllers/HelpController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudyDirectionDbWebApp.Controllers
+{
+    public class HelpController : Controller
+    {
+        // GET: Help
+        public ActionResult Purpose()
+        {
+            return View();
+        }
+
+        // GET: Help
+        public ActionResult RuntimeConditions()
+        {
+            return View();
+        }
+        // GET: Help
+        public ActionResult Running()
+        {
+            return View();
+        }
+        // GET: Help
+        public ActionResult ExecutionOrder()
+        {
+            return View();
+        }
+        // GET: Help
+        public ActionResult FunctionsDescription()
+        {
+            return View();
+        }
+        // GET: Help
+        public ActionResult OperatorMessages()
+        {
+            return View();
+        }
+    }
+}
